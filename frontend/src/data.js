bf70e14979f527abb03b35a929830bed37bc7f29
nielsroux <nielsroux@gmail.com>
2022-02-28 12:11:30 +0100

    Shared project on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+export const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Montre Slim',
+            category:'montre',
+            image:'/images/products/1.jpg',
+            price:120,
+            brand:'Slein',
+            rating:3.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Casque Audio',
+            category:'audio',
+            image:'/images/products/2.jpg',
+            price:300,
+            brand:'Slein',
+            rating:5,
+            numReviews:5,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Polaroid',
+            category:'appareil-photo',
+            image:'/images/products/3.jpg',
+            price:120,
+            brand:'Slein',
+            rating:3.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Pot de fleur',
+            category:'décoration',
+            image:'/images/products/4.jpg',
+            price:50,
+            brand:'Slein',
+            rating:4.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Rayban',
+            category:'lunette',
+            image:'/images/products/5.jpg',
+            price:150,
+            brand:'Slein',
+            rating:5,
+            numReviews:15,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Nike Basket',
+            category:'chaussure',
+            image:'/images/products/6.jpg',
+            price:220,
+            brand:'Slein',
+            rating:4.5,
+            numReviews:30,
+            description: 'High quality product'
+        }
+    ]
+};
+
+//export default data;
