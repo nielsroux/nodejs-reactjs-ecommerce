c022bb6b0a2901eb1257a86b841d67bc68fdd56b
nielsroux <nielsroux@gmail.com>
2022-02-28 16:27:38 +0100

    Add backend folder - NodeJs Server

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Montre Slim',
+            category:'montre',
+            image:'/images/products/1.jpg',
+            price:120,
+            countInStock:5,
+            brand:'Slein',
+            rating:3.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Casque Audio',
+            category:'audio',
+            image:'/images/products/2.jpg',
+            price:300,
+            countInStock:10,
+            brand:'Slein',
+            rating:5,
+            numReviews:5,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Polaroid',
+            category:'appareil-photo',
+            image:'/images/products/3.jpg',
+            price:120,
+            countInStock:3,
+            brand:'Slein',
+            rating:4.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Pot de fleur',
+            category:'décoration',
+            image:'/images/products/4.jpg',
+            price:50,
+            countInStock:5,
+            brand:'Slein',
+            rating:2.5,
+            numReviews:10,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Rayban',
+            category:'lunette',
+            image:'/images/products/5.jpg',
+            price:150,
+            countInStock:15,
+            brand:'Slein',
+            rating:0.5,
+            numReviews:15,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Nike Basket',
+            category:'chaussure',
+            image:'/images/products/6.jpg',
+            price:220,
+            countInStock:0,
+            brand:'Slein',
+            rating:4.5,
+            numReviews:30,
+            description: 'High quality product'
+        }
+    ]
+};
+
+export default data;
